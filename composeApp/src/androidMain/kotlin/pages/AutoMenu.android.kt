f9b18273fa15d2b6b9b58bb623d8856f35f01f2a
Someone1121 <eli.tribhuwan12@gmail.com>
2025-01-27 14:12:17 -0800

    Made checkbox function and added A-Stop

--- composeApp/src/androidMain/kotlin/pages/AutoMenu.android.kt
+++ composeApp/src/androidMain/kotlin/pages/AutoMenu.android.kt
@@ -1,33 +1,21 @@
 package pages
 
-import android.graphics.Paint.Align
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bumble.appyx.components.backstack.BackStack
 import com.bumble.appyx.components.backstack.operation.pop
-import com.bumble.appyx.components.backstack.operation.push
 import composables.CheckBox
+import composables.TriStateCheckBox
 //import composables.AutoCheckboxesHorizontal
 //import composables.AutoCheckboxesVertical
 import composables.EnumerableValueAuto
-import defaultOnPrimary
-import defaultSecondary
 import exportScoutData
 import keyboardAsState
 import nodes.*
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 actual fun AutoMenu(
@@ -91,7 +79,7 @@ actual fun AutoMenu(
                         .fillMaxWidth()
                 ) {
 
-                    CheckBox(
+                    TriStateCheckBox(
                         label = "C3",
                         ifChecked = coral3Collected,
                         modifier = Modifier
@@ -99,7 +87,7 @@ actual fun AutoMenu(
                             .fillMaxSize()
                     )
 
-                    CheckBox(
+                    TriStateCheckBox(
                         label = "A3",
                         ifChecked = algae3Collected,
                         modifier = Modifier
@@ -115,7 +103,7 @@ actual fun AutoMenu(
                         .fillMaxWidth()
                 ) {
 
-                    CheckBox(
+                    TriStateCheckBox(
                         label = "C2",
                         ifChecked = coral2Collected,
                         modifier = Modifier
@@ -123,7 +111,7 @@ actual fun AutoMenu(
                             .fillMaxSize()
                     )
 
-                    CheckBox(
+                    TriStateCheckBox(
                         label = "A2",
                         ifChecked = algae2Collected,
                         modifier = Modifier
@@ -139,7 +127,7 @@ actual fun AutoMenu(
                         .fillMaxWidth()
                 ) {
 
-                    CheckBox(
+                    TriStateCheckBox(
                         label = "C1",
                         ifChecked = coral1Collected,
                         modifier = Modifier
@@ -147,7 +135,7 @@ actual fun AutoMenu(
                             .fillMaxSize()
                     )
 
-                    CheckBox(
+                    TriStateCheckBox(
                         label = "A1",
                         ifChecked = algae1Collected,
                         modifier = Modifier
@@ -297,7 +285,13 @@ actual fun AutoMenu(
                         .weight(1f)
                         .fillMaxWidth()
                 )
-
+                CheckBox(
+                    label = "A-stop",
+                    ifChecked = autoStop,
+                    modifier = Modifier
+                        .weight(0.5f)
+                        .fillMaxWidth()
+                )
             }
 
         }
