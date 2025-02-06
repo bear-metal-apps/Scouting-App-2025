b43599f00a529a8e8c051dd386038fa36b917d62
Jacob McKee <jacobmckee06@gmail.com>
2025-02-06 15:08:46 -0800

    Delete unimportant comment

--- composeApp/src/commonMain/kotlin/FileMaker.kt
+++ composeApp/src/commonMain/kotlin/FileMaker.kt
@@ -6,8 +6,6 @@ fun getJsonFromMatchHash(): JSONObject {
     val jsonObject = JSONObject()
     teamDataArray.keys.forEach {
         jsonObject.put(it.toString(), teamDataArray[it])
-//        val map = teamDataArray[it]
-//        (jsonObject[it.toString()] as JSONArray).put(map)
     }
 
     return jsonObject
