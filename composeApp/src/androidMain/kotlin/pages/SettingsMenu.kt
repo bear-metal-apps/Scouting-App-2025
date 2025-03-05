907b8dac17c593e6d8fc8a13e9fbb6cca7d60828
Jack J <sailboat31@outlook.com>
2025-03-04 16:31:19 -0800

    Make settings look monumentally better

--- composeApp/src/androidMain/kotlin/pages/SettingsMenu.kt
+++ composeApp/src/androidMain/kotlin/pages/SettingsMenu.kt
@@ -5,14 +5,15 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ChevronLeft
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,56 +66,60 @@ actual fun SettingsMenu(
             thickness = 3.dp,
             modifier = Modifier.padding(8.dp)
         )
-        Text(
-            text = "Current Rank:",
-            fontSize = 35.sp,
-            color = getCurrentTheme().onPrimary,
-        )
-        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
-
-        Text(
-            text = "TODO put ranked photo for the scout right here and an xp bar",
-            fontSize = 18.sp,
-            color = getCurrentTheme().onPrimary,
-            modifier = Modifier,
-        )
-
-        Spacer(modifier = Modifier.padding(20.dp))
-        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
+//        Text(
+//            text = "Current Rank:",
+//            fontSize = 35.sp,
+//            color = getCurrentTheme().onPrimary,
+//        )
+//        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
+//
+//        Text(
+//            text = "TODO put ranked photo for the scout right here and an xp bar",
+//            fontSize = 18.sp,
+//            color = getCurrentTheme().onPrimary,
+//            modifier = Modifier,
+//        )
+//
+//        Spacer(modifier = Modifier.padding(20.dp))
+//        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
         Text(
             text = "Appearance",
-            fontSize = 35.sp,
+            fontSize = 24.sp,
             color = getCurrentTheme().onPrimary,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
+                .padding(8.dp)
         )
-        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
+        HorizontalDivider(
+            color = defaultPrimaryVariant,
+            thickness = 3.dp,
+            modifier = Modifier.padding(8.dp)
+        )
+//        Row {
+//            DropdownMenu(
+//                expanded = themeExpanded,
+//                onDismissRequest = { themeExpanded = false },
+//            ) {
+//            }
+//
+//            OutlinedButton(
+//                onClick = {
+//                    themeExpanded = true
+//                },
+//                colors = ButtonDefaults.outlinedButtonColors(
+//                    backgroundColor = getCurrentTheme().secondary,
+//                    contentColor = getCurrentTheme().onPrimary
+//                ),
+//                shape = RoundedCornerShape(5.dp),
+//            ) {
+//                Text(
+//                    text = "Theme",
+//                    color = getCurrentTheme().onPrimary
+//                )
+//            }
+//        }
+
         Row {
-            DropdownMenu(
-                expanded = themeExpanded,
-                onDismissRequest = { themeExpanded = false },
-            ) {
-            }
-
-            OutlinedButton(
-                onClick = {
-                    themeExpanded = true
-                },
-                colors = ButtonDefaults.outlinedButtonColors(
-                    backgroundColor = getCurrentTheme().secondary,
-                    contentColor = getCurrentTheme().onPrimary
-                ),
-                shape = RoundedCornerShape(5.dp),
-            ) {
-                Text(
-                    text = "Theme",
-                    color = getCurrentTheme().onPrimary
-                )
-            }
-        }
-
-        Row {
-
             Switch(
                 checked = effectsChecked,
                 onCheckedChange = { effectsChecked = !effectsChecked },
@@ -125,36 +130,44 @@ actual fun SettingsMenu(
                     uncheckedTrackColor = getCurrentTheme().onPrimary,
                 ),
                 modifier = Modifier
-                    .scale(2f)
-                    .padding(25.dp)
-
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically)
             )
             Text(
                 text = "Effects",
-                fontSize = 25.sp,
                 color = getCurrentTheme().onPrimary,
                 modifier = Modifier
-                    .padding(28.dp)
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically)
             )
         }
 
-        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
-
-        Text(
-            text = "Accessibility",
-            fontSize = 35.sp,
-            color = getCurrentTheme().onPrimary,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-        )
-        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
-
-        Text(
-            text = "TODO Put algae and coral column move-ability to here",
-            fontSize = 18.sp,
-            color = getCurrentTheme().onPrimary,
-            modifier = Modifier,
-        )
+//        HorizontalDivider(
+//            color = defaultPrimaryVariant,
+//            thickness = 3.dp,
+//            modifier = Modifier.padding(8.dp)
+//        )
+//        
+//        Text(
+//            text = "Accessibility",
+//            fontSize = 24.sp,
+//            color = getCurrentTheme().onPrimary,
+//            modifier = Modifier
+//                .align(Alignment.CenterHorizontally)
+//                .padding(8.dp)
+//        )
+//        HorizontalDivider(
+//            color = defaultPrimaryVariant,
+//            thickness = 3.dp,
+//            modifier = Modifier.padding(8.dp)
+//        )
+        //
+//        Text(
+//            text = "TODO Put algae and coral column move-ability to here",
+//            fontSize = 18.sp,
+//            color = getCurrentTheme().onPrimary,
+//            modifier = Modifier,
+//        )
         Row {
             Switch(
                 checked = miniMinus.value,
@@ -166,38 +179,41 @@ actual fun SettingsMenu(
                     uncheckedTrackColor = getCurrentTheme().onPrimary,
                 ),
                 modifier = Modifier
-                    .scale(2f)
-                    .padding(25.dp)
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically)
 
             )
             Text(
                 text = "Mini-Minus Buttons",
-                fontSize = 25.sp,
                 color = getCurrentTheme().onPrimary,
                 modifier = Modifier
-                    .padding(28.dp)
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically)
             )
         }
-        HorizontalDivider(thickness = 2.dp, color = getCurrentTheme().primaryVariant)
-        OutlinedButton(
+        
+        HorizontalDivider(
+            color = defaultPrimaryVariant,
+            thickness = 3.dp,
+            modifier = Modifier.padding(8.dp)
+        )
+
+        androidx.compose.material3.OutlinedButton(
+            border = BorderStroke(3.dp, Color.Yellow),
+            shape = RoundedCornerShape(12.dp),
+            colors = androidx.compose.material3.ButtonDefaults.buttonColors(containerColor = defaultSecondary),
             onClick = {
                 effectsChecked = false
                 theme = themeDefault()
             },
-            colors = ButtonDefaults.outlinedButtonColors(
-                backgroundColor = getCurrentTheme().secondary,
-                contentColor = getCurrentTheme().onPrimary
-            ),
-            shape = RoundedCornerShape(5.dp),
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
+                .padding(8.dp, 0.dp, 8.dp, 0.dp)
         ) {
             Text(
                 text = "Reset to Default",
-                color = getCurrentTheme().onPrimary,
-                fontSize = 35.sp,
+                color = getCurrentTheme().onPrimary
             )
         }
-
     }
 }
